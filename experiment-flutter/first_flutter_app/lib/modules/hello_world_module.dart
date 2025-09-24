248fb109d4f312c651dc4bfeebebce4ac2365c9a
Widi Arrohman <58793672+widiarrohman1234@users.noreply.github.com>
2025-09-24 08:27:19 +0700

    Create hello_world_module.dart

---
+++ experiment-flutter/first_flutter_app/lib/modules/hello_world_module.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HelloWorldFlutter extends StatelessWidget {
+  const HelloWorldFlutter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [Text("Hello World")]);
+  }
+}
