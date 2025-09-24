abed9eea883750c3ee17b5ed06b0bdd15731d1ab
Widi Arrohman <58793672+widiarrohman1234@users.noreply.github.com>
2025-09-24 08:24:57 +0700

    UPDATE: Implementasi library

--- IIIA/implementasi-library/main.dart
+++ IIIA/implementasi-library/main.dart
@@ -1,22 +1,22 @@
-import 'utils.dart';
-import 'package:intl/date_symbol_data_local.dart';
+// import 'utils.dart';
+// import 'package:intl/date_symbol_data_local.dart';
 
-void main() async {
-  await initializeDateFormatting('id_ID', null);
-  // formatter.dart
-  // double harga = 1500000.50;
-  // print('Harga produk: ${formatCurrency(harga)}');
-  DateTime tanggalSekarang = DateTime.now();
-  print("Tanggal Hari ini: ${formatDate(tanggalSekarang)}");
+// void main() async {
+//   await initializeDateFormatting('id_ID', null);
+//   // formatter.dart
+//   // double harga = 1500000.50;
+//   // print('Harga produk: ${formatCurrency(harga)}');
+//   DateTime tanggalSekarang = DateTime.now();
+//   print("Tanggal Hari ini: ${formatDate(tanggalSekarang)}");
 
-  // validator.dart
-  // String email1 = 'widiarrohman1234@gmail.com';
-  // String email2 = "salahmenyalahgmail.com";
-  // print('Email valid? ${isValidEmail(email1)}');
-  // print('Apakah Email valid? ${isValidEmail(email2)}');
+//   // validator.dart
+//   // String email1 = 'widiarrohman1234@gmail.com';
+//   // String email2 = "salahmenyalahgmail.com";
+//   // print('Email valid? ${isValidEmail(email1)}');
+//   // print('Apakah Email valid? ${isValidEmail(email2)}');
 
-  // calculator.dart
-  // double hargaDiskon = calculateDiscount(100000, 20);
-  // print("isi data hargaDiskon: $hargaDiskon");
-  // print("Harga setelah diskon: ${formatCurrency(hargaDiskon)}");
-}
+//   // calculator.dart
+//   // double hargaDiskon = calculateDiscount(100000, 20);
+//   // print("isi data hargaDiskon: $hargaDiskon");
+//   // print("Harga setelah diskon: ${formatCurrency(hargaDiskon)}");
+// }
--- IIIA/implementasi-library/src/formatter.dart
+++ IIIA/implementasi-library/src/formatter.dart
@@ -1,15 +1,15 @@
-import 'package:intl/intl.dart';
+// import 'package:intl/intl.dart';
 
-String formatCurrency(double amount) {
-  final format = NumberFormat.currency(
-    locale: 'id_ID',
-    symbol: 'Rp.',
-    decimalDigits: 0,
-  );
+// String formatCurrency(double amount) {
+//   final format = NumberFormat.currency(
+//     locale: 'id_ID',
+//     symbol: 'Rp.',
+//     decimalDigits: 0,
+//   );
 
-  return format.format(amount);
-}
+//   return format.format(amount);
+// }
 
-String formatDate(DateTime date) {
-  return DateFormat('dd MMMM yyyy', 'id_ID').format(date);
-}
+// String formatDate(DateTime date) {
+//   return DateFormat('dd MMMM yyyy', 'id_ID').format(date);
+// }
